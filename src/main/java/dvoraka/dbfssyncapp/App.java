1c4cd52638b3ff206f05346283946c956eff5061
dvoraka <alen.dvorak@gmail.com>
2019-11-28 13:40:02 +0100

    #2 Prepare app

--- src/main/java/dvoraka/dbfssyncapp/App.java
+++ src/main/java/dvoraka/dbfssyncapp/App.java
@@ -1,7 +1,21 @@
 package dvoraka.dbfssyncapp;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("App");
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner() {
+        return args -> {
+            System.out.println("App");
+        };
     }
 }
